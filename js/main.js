30af15b9810fe5012ad2ae7928ff7e083082cefc
amber_p <panianamber@gmail.com>
2020-05-23 17:48:25 -0600

    finalized landing page

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+
+// Image List
+images[0] = "img/austin.png";
+images[1] = "img/personalsite-1.png";
+images[2] = "img/portfolio-wireframe.png";
+images[3] = "img/unplugged-project.png";
+images[4] = "img/rogue.png";
+
+// Change Image
+function changeImg() {
+  document.slide.src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload = changeImg;
